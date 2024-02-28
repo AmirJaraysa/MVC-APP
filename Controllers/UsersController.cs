22d5cd57fb85495d3a4b7abc1e71c68642f24739
AmirJaraysa <amirjaraysa@gmail.com>
2024-02-27 19:50:27 -0500

    First commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AJaraysa.Models;
+
+namespace AJaraysa.Controllers
+{
+    public class UsersController : Controller
+    {
+        UsersContext uc = new();
+        public IActionResult AJView()
+        {
+            return View(uc.JaraysaAtables.ToList());
+        }
+    }
+}
---
+++ Models/JaraysaAtable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AJaraysa.Models;
+
+public partial class JaraysaAtable
+{
+    public int UserId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? UserEmail { get; set; }
+}
